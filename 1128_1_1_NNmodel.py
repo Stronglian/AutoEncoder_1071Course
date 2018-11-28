d6865bab53a8472792a6e646ac24e61a0ef387f4
Stronglian <strong.85.wu@gmail.com>
2018-11-28 19:07:54 +0800

    Create 1128_1_1_NNmodel.py

---
+++ 1128_1_1_NNmodel.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 14:00:23 2018
+
+@author: StrongPria
+"""
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.optim import SGD
+
+class Net(nn.Moudle):
+    def __init__(self):
+        super(Net, self).__init__()
+        return
+    def forward(self, x):
+        
+        return
+    
